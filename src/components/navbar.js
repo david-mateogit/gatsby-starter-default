807459cd063afa83da1982c3f2bf200f03fb0226
david-mateogit <davidmateo@outlook.com>
2020-01-19 12:30:43 -0400

    mobile view fix

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -55,10 +55,14 @@ const Navbar = () => (
           rel="noopener noreferrer"
           href="https://www.google.com/maps/dir/19.4436952,-70.6811497/Clinica+Dental+Dra+Claritza+Sanchez,+Carretera+Sos%C3%BAa+cabarete+esq,+Dr.+Rosen,+Sos%C3%BAa+57000/@19.6035907,-70.7404661,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x8eb1e3708f32166f:0x2de2f93b48a5594f!2m2!1d-70.5137815!2d19.7635945"
           css={css`
+            font-size: 1.5rem;
             text-decoration: none;
-            color: #f0e3ff;
+            color: #3e206d;
+            font-weight: bold;
+            margin-top: 2px;
+
             @media (max-width: 550px) {
-              display: none;
+              font-size: 1rem;
             }
           `}
         >
